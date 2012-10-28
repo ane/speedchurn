4d014d93eb4707e275f27d12157d37d89a280dde
ane <ane@iki.fi>
2012-10-28 21:03:12 +0200

    Update LineChunks doc.

--- chunks.go
+++ chunks.go
@@ -19,7 +19,9 @@ func (c ChunkSpec) String() string {
 	return fmt.Sprintf("ChunkSpec { offset: %d, length: %d }", c.offset, c.length)
 }
 
-func lineChunks(numChunks int, file string) []ChunkSpec {
+// LineChunks splits the file into numChunks parts, that begin on a newline.
+// The chunks are guaranteed to end on a newline for all but the last chunk.
+func LineChunks(numChunks int, file string) []ChunkSpec {
 	fi, err := os.Open(file)
 	if err != nil {
 		panic("can't open file: " + file)
@@ -29,26 +31,24 @@ func lineChunks(numChunks int, file string) []ChunkSpec {
 	info, err := os.Stat(file)
 	totalSize := info.Size()
 	chunkSize := totalSize / int64(numChunks)
-	//fmt.Println("File size is ", totalSize)
-	//fmt.Println("Chunk size is ", chunkSize)
 	specs := make([]ChunkSpec, numChunks)
 	offset := int64(0)
 	br := bufio.NewReader(fi)
+
 	for i := 0; i < numChunks; i++ {
 		currentChunkSize := chunkSize
 		newOffset := offset + currentChunkSize
-		//fmt.Println("Starting at", offset, ", seeking to ", newOffset)
+
 		_, err := fi.Seek(newOffset, 0)
 		if err == io.EOF {
 			specs[i] = ChunkSpec{offset, totalSize - offset}
 			break
 		}
 
+		// find newline
 		data, err := br.ReadBytes('\n')
 		untilNewLine := int64(len(data))
 		currentChunkSize += untilNewLine
-		//fmt.Println("Found newline at position", newOffset + untilNewLine, "(",
-		//newOffset, "+", untilNewLine, ")")
 
 		specs[i] = ChunkSpec{offset, currentChunkSize}
 
