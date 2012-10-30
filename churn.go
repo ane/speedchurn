d70027012d56167a058cd7c54099a1cab1446971
ane <ane@iki.fi>
2012-10-30 20:00:20 +0200

    Add time tracking.

--- churn.go
+++ churn.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"time"
+	"fmt"
+)
+
 // Churn processes a log file first by chunking it into parts and then mapreducing
 // all of these simultaneously.
 func Churn(file string, ch chan ChanStats) {
@@ -7,7 +12,11 @@ func Churn(file string, ch chan ChanStats) {
 	chunks := LoadChunks(file, specs)
 
 	c := ChanStats{channelName: file, chunks: chunks, matcher: new(IrssiMatcher)}
+
+	t := time.Now()
 	c.stats = MapReduce(MapChunk, ReduceChunks, GetChunks(c), 4).(StatsChunk)
+	dur := time.Since(t)
+	fmt.Println(file, "complete in", dur)
 
 	ch <- c
 	wg.Done()
