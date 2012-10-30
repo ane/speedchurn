df950229150506c2a39fbb75e0c6c843377bdfb9
ane <ane@iki.fi>
2012-10-30 19:52:40 +0200

    Add matching multiplexing, it's now concurrent.

--- analysis.go
+++ analysis.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"bytes"
-	"fmt"
 	"io"
+	"reflect"
+	"runtime"
+	"sync"
 )
 
 func ReduceChunks(source chan interface{}, output chan interface{}) {
@@ -17,14 +19,50 @@ func ReduceChunks(source chan interface{}, output chan interface{}) {
 func MapChunk(source interface{}, output chan interface{}) {
 	chunk := source.(Chunk)
 	buffer := bytes.NewBuffer(chunk)
+	impStats := ImpertinentStats{}
 	for {
 		line, err := buffer.ReadBytes('\n')
 		if err != nil && err == io.EOF {
 			break
+		} else {
+			what := Match(line, matcher)
+			switch what.(type) {
+			default:
+				//fmt.Println("type is %T", typ)
+			case bool:
+				impStats.dayChanges += 1
+			case Topic:
+				impStats.topicChanges += 1
+			}
 		}
-		fmt.Println(string(line))
 	}
-	output <- StatsChunk{ImpertinentStats{}}
+	output <- StatsChunk{impStats}
+}
+
+func Match(line []byte, m Matcher) interface{} {
+	methods := []func([]byte, Matcher) (bool, interface{}){MatchDayChange, MatchTopicChange}
+	var group sync.WaitGroup
+	hit := make(chan interface{})
+	// multiplex the matching to all matcher methods
+	for _, fun := range methods {
+		go func(f func([]byte, Matcher) (bool, interface{})) {
+			match, res := f(line, m)
+			group.Add(1)
+			if match == true {
+				hit <- res
+			}
+			group.Done()
+		}(fun)
+	}
+	go func() {
+		group.Wait()
+		close(hit)
+	}()
+	res, ok := <-hit
+	if ok {
+		return res
+	}
+	return nil
 }
 
 func GetChunks(c ChanStats) chan interface{} {
@@ -38,3 +76,8 @@ func GetChunks(c ChanStats) chan interface{} {
 	}()
 	return chunkChan
 }
+
+func GetFunctionName(i interface{}) string {
+	return runtime.FuncForPC(reflect.ValueOf(i).Pointer()).Name()
+}
+
